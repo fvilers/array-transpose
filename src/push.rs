17dbbd54d76ab3270ba23acf4565c263cd0a533c
Fabian Vilers <fabian.vilers@dev-one.com>
2023-04-23 09:24:54 +0200

    Add down and up directions for Push trait

--- src/push.rs
+++ src/push.rs
@@ -1,8 +1,10 @@
 use crate::{coords_to_index, index_to_coords, Transpose};
 
 pub enum PushDirection {
+    Down,
     Left,
     Right,
+    Up,
 }
 
 pub trait Push<T, const N: usize>
@@ -22,8 +24,10 @@ where
         self.transpose(|index| {
             let (x, y) = index_to_coords(index, len);
             match direction {
+                PushDirection::Down => coords_to_index((x + len + 1) % len, y, len),
                 PushDirection::Left => coords_to_index(x, (y + len - 1) % len, len),
                 PushDirection::Right => coords_to_index(x, (y + len + 1) % len, len),
+                PushDirection::Up => coords_to_index((x + len - 1) % len, y, len),
             }
         })
     }
@@ -32,6 +36,14 @@ where
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn push_down_should_return_a_pushed_array_of_1_element() {
+        let actual = [1];
+        let result = actual.push(PushDirection::Down);
+
+        assert_eq!([1], result);
+    }
 
     #[test]
     fn push_left_should_return_a_pushed_array_of_1_element() {
@@ -47,6 +59,22 @@ mod tests {
         let result = actual.push(PushDirection::Right);
 
         assert_eq!([1], result);
+    }
+
+    #[test]
+    fn push_up_should_return_a_pushed_array_of_1_element() {
+        let actual = [1];
+        let result = actual.push(PushDirection::Up);
+
+        assert_eq!([1], result);
+    }
+
+    #[test]
+    fn push_down_should_return_a_pushed_array_of_4_elements() {
+        let actual = [1, 2, 3, 4];
+        let result = actual.push(PushDirection::Down);
+
+        assert_eq!([3, 4, 1, 2], result);
     }
 
     #[test]
@@ -66,6 +94,22 @@ mod tests {
     }
 
     #[test]
+    fn push_up_should_return_a_pushed_array_of_4_elements() {
+        let actual = [1, 2, 3, 4];
+        let result = actual.push(PushDirection::Up);
+
+        assert_eq!([3, 4, 1, 2], result);
+    }
+
+    #[test]
+    fn push_down_should_return_a_pushed_array_of_9_elements() {
+        let actual = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        let result = actual.push(PushDirection::Down);
+
+        assert_eq!([7, 8, 9, 1, 2, 3, 4, 5, 6], result);
+    }
+
+    #[test]
     fn push_left_should_return_a_pushed_array_of_9_elements() {
         let actual = [1, 2, 3, 4, 5, 6, 7, 8, 9];
         let result = actual.push(PushDirection::Left);
@@ -79,5 +123,13 @@ mod tests {
         let result = actual.push(PushDirection::Right);
 
         assert_eq!([3, 1, 2, 6, 4, 5, 9, 7, 8], result);
+    }
+
+    #[test]
+    fn push_up_should_return_a_pushed_array_of_9_elements() {
+        let actual = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        let result = actual.push(PushDirection::Up);
+
+        assert_eq!([4, 5, 6, 7, 8, 9, 1, 2, 3], result);
     }
 }
