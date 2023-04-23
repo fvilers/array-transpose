6a19b6177298f7b84b75817ec76c740f8637e7d8
Fabian Vilers <fabian.vilers@dev-one.com>
2023-04-23 08:43:24 +0200

    Add the Transpose trait

---
+++ src/transpose.rs
@@ -0,0 +1,33 @@
+use std::ops::Index;
+
+pub trait Transpose<T, const N: usize>: Index<usize, Output = T>
+where
+    T: Copy + Default,
+{
+    fn transpose<F>(&self, f: F) -> [T; N]
+    where
+        F: Fn(usize) -> usize,
+    {
+        let mut result = [T::default(); N];
+
+        (0..N).for_each(|index| result[f(index)] = *self.index(index));
+        result
+    }
+}
+
+impl<T, const N: usize> Transpose<T, N> for [T; N] where T: Copy + Default {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn transpose_should_shift_elements_to_the_right() {
+        let array = [1, 2, 3, 4];
+
+        assert_eq!(
+            array.transpose(|index| (index + 1 + array.len()) % array.len()),
+            [4, 1, 2, 3]
+        );
+    }
+}
